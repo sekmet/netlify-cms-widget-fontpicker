d1ef6ef99f7c007fdab59f421365347c1fc0684a
Carlos Guimaraes <sekmet@open4g.com>
2019-08-19 05:41:18 -0300

    fix: allow multiple font selectors in same page

--- src/FontpickerControl.js
+++ src/FontpickerControl.js
@@ -3,66 +3,70 @@ import { jsx } from '@emotion/core';
 import React from 'react';
 import PropTypes from 'prop-types';
 import FontPicker from "font-picker-react";
+const uniqid = require('uniqid');
 
 export default class FontpickerControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeFontFamily: 'Open Sans',
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeFontFamily: 'Open Sans',
+        };
+    }
+
+    static propTypes = {
+        onChange: PropTypes.func.isRequired,
+        forID: PropTypes.string,
+        value: PropTypes.node,
+        classNameWrapper: PropTypes.string.isRequired,
+        setActiveStyle: PropTypes.func.isRequired,
+        setInactiveStyle: PropTypes.func.isRequired,
+        apiKey: PropTypes.string,
+        activeFontFamily: PropTypes.string,
+        activeFontCategory: PropTypes.string,
     };
-  }
 
-  static propTypes = {
-    onChange: PropTypes.func.isRequired,
-    forID: PropTypes.string,
-    value: PropTypes.node,
-    classNameWrapper: PropTypes.string.isRequired,
-    setActiveStyle: PropTypes.func.isRequired,
-    setInactiveStyle: PropTypes.func.isRequired,
-    apiKey: PropTypes.string,
-    activeFontFamily: PropTypes.string,
-    activeFontCategory: PropTypes.string,
-  };
+    static defaultProps = {
+        value: ''
+    };
 
-  static defaultProps = {
-    value: ''
-  };
+    render() {
+        const {
+            forID,
+            value,
+            onChange,
+            classNameWrapper,
+            setActiveStyle,
+            setInactiveStyle,
+        } = this.props;
 
-  render() {
-    const {
-      forID,
-      value,
-      onChange,
-      classNameWrapper,
-      setActiveStyle,
-      setInactiveStyle,
-    } = this.props;
+        const pId = uniqid();
 
-    return (
-      <div style={{width: '100%'}}>
-        <FontPicker
-          id={forID}
-          apiKey={process.env.GATSBY_GOOGLE_FONTS_API_KEY ? process.env.GATSBY_GOOGLE_FONTS_API_KEY : process.env.GOOGLE_FONTS_API_KEY}
-          className={classNameWrapper}
-          activeFontFamily={value || this.state.activeFontFamily }
-          value={value || '' }
-          onFocus={setActiveStyle}
-          onBlur={setInactiveStyle}
-          onChange={nextFont => {
-            this.setState({
-              activeFontFamily: nextFont.family,
-              activeFontCategory: nextFont.category
-            })
-            //console.log(this.state.activeFontFamily, nextFont.category)
-            //return onChange(`('${this.state.activeFontFamily}', ${nextFont.category ? nextFont.category : 'sans-serif'})`)
-            return onChange(`${this.state.activeFontFamily}`)
-          }
-          }
-        />
+        return (
+            <div>
+                <FontPicker
+                    id={forID}
+                    pickerId={pId}
+                    apiKey={process.env.GATSBY_GOOGLE_FONTS_API_KEY ? process.env.GATSBY_GOOGLE_FONTS_API_KEY : process.env.GOOGLE_FONTS_API_KEY} //'AIzaSyDk-dW0JU7GFHrX3Pg_cgTUvJwH3G2FPhs'
+                    className={classNameWrapper}
+                    activeFontFamily={value || this.state.activeFontFamily }
+                    value={value || '' }
+                    onFocus={setActiveStyle}
+                    onBlur={setInactiveStyle}
+                    onChange={nextFont => {
+                        this.setState({
+                            activeFontFamily: nextFont.family,
+                            activeFontCategory: nextFont.category
+                        })
+                        //console.log(this.state.activeFontFamily, nextFont.category)
+                        //return onChange(`('${this.state.activeFontFamily}', ${nextFont.category ? nextFont.category : 'sans-serif'})`)
+                        return onChange(`${this.state.activeFontFamily}`)
+                    }
+                    }
+                />
 
-        <p className="apply-font" style={{paddingTop: '9px'}}>The font will be applied to this text.</p>
+                <p className={`apply-font-${pId}`} style={{paddingTop: '9px'}}>The font will be applied to this text.</p>
 
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
